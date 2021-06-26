24b0082c59f13bd73650208f669cc5d351ccf8c0
Ralf Krauze <ralf.krauze@yahoo.com>
2021-06-26 20:45:19 +0100

    User and Event model set up and creator_id associated

--- db/migrate/20210626031251_create_events.rb
+++ db/migrate/20210626031251_create_events.rb
@@ -3,7 +3,6 @@ class CreateEvents < ActiveRecord::Migration[6.1]
     create_table :events do |t|
       t.string :name
       t.string :date
-      t.references :User, null: false, foreign_key: true
 
       t.timestamps
     end
